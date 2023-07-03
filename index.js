6bb555dc808220928c9446257e90853f95fd59c1
devmhmdio <mohammedrafique23@gmail.com>
2023-07-03 11:03:47 +0530

    Setting up API

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const router = require('./router');
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
