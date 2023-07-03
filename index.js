69f37c33c28988a455fe17e71a5ab17eaebb7071
devmhmdio <mohammedrafique23@gmail.com>
2023-07-03 11:30:42 +0530

    Complete setting up connection with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = require('./router');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 app.use(router);
 
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
