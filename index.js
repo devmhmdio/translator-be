4a271c5eed1bc79d6d51f3ce36ecdd5cead57aba
devmhmdio <mohammedrafique23@gmail.com>
2023-07-12 17:55:37 +0530

    Changed cors url

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const app = express();
 const server = require('http').Server(app);
 const io = socketIo(server, {
   cors: {
-    // origin: 'https://translator-fe.vercel.app',
-    origin: 'http://localhost:3000',
+    origin: 'https://translator-fe.vercel.app',
+    // origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
     allowedHeaders: ['my-custom-header'],
     credentials: true,
