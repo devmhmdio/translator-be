040ebce73d127d6769bbb34f1c6cbfe683e3c9e0
devmhmdio <mohammedrafique23@gmail.com>
2023-07-20 02:36:28 +0530

    updating cors policy code

--- index.js
+++ index.js
@@ -15,7 +15,17 @@ const port = process.env.PORT || 8000;
 const app = express();
 const server = require('http').Server(app);
 // const io = socketIo(server);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    // origin: 'https://translator-fe.vercel.app',
+    origin: 'https://www.waaztranslations.com',
+    // origin: 'http://localhost:3000',
+    // origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+});
 app.use(
   cors({
     // origin: 'https://translator-fe.vercel.app',
